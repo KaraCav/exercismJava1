f70c1ccd18c07c1e784b3da7fa32e3aa160c10d3
Kara Cavanaugh <kcavanaugh@rationalenterprise.com>
2019-04-05 16:32:20 -0400

    Number 3 Reverse String

---
+++ reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,12 @@
+class ReverseString {
+
+    String reverse(String inputString) {
+        char[] theInput = inputString.toCharArray();
+        String toReturn = "";
+
+        for(int i = theInput.length-1; i >= 0; i--){
+            toReturn += theInput[i];
+        }
+        return String.valueOf(toReturn);
+    }
+}
